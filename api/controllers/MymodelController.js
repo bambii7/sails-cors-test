43384f5b740a375f984b119d0a247ec280251b11
Alexis <alexis@cvofhope.com>
2015-07-02 22:05:45 +0200

    add basic model

---
+++ api/controllers/MymodelController.js
@@ -0,0 +1,11 @@
+/**
+ * MymodelController
+ *
+ * @description :: Server-side logic for managing mymodels
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
